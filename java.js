f0ce4ed1fc8827521784e11a19642f961b397f36
Ibtassamfarooqui <156608991+Ibtassamfarooqui@users.noreply.github.com>
2024-01-21 21:02:33 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+let age;
+let isStudent;
+age = 24;
+isStudent = true;
+console.log("Age: ", age);
+console.log("Is Student: ", isStudent);
+
+window.onload = function() {
+    alert('Welcome to our Web page! I am happy to see you here.');
+   };
+
+let length = 21;
+let width = 9;
+
+let area = length * width;
+
+console.log("The area of the rectangle is: " + area);
+let userName = prompt("Please enter your name:");
+let greeting = "Hello, " + userName + "! Welcome to our website!";
+alert(greeting);
